8a329e57723db050db7bb5ea71d90626a03162ff
EC2 Default User <ec2-user@ip-172-31-1-19.us-east-2.compute.internal>
2019-04-20 03:21:54 +0000

    Did issue #2. Review needed.

---
+++ imports/startup/server/serverStartup.js
@@ -0,0 +1,18 @@
+import { Meteor } from 'meteor/meteor';
+import { Roles } from 'meteor/alanning:roles';
+
+if(Meteor.users.find().count() === 0) {
+    console.log("Creating Default User");
+    Meteor.settings.defaultUsers.forEach((user) => {
+        let newUserId = Accounts.createUser({
+            "password":user.password,
+            "username":user.username,
+            "email":user.email
+        });
+        
+        Roles.addUsersToRoles(newUserId, ['superadmin']);
+    });
+} else {
+    console.log("Users Already Found");
+}
+
